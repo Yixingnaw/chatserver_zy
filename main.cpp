1e14ca0c2f641b03ea6f1a64e014036e933230e2
Yixingnaw <lian89093@gmail.com>
2024-03-10 05:52:04 +0800

    test_vscode

--- main.cpp
+++ main.cpp
@@ -23,7 +23,8 @@ int main(int argc,char **argv)
   LOG_INFO << "info level log message";
   LOG_WARN << "warn level log message";
   LOG_ERROR << "error level log message";
-
+     int x;
+     int xxxx;
        User ass(1,"2","2","3");
        UserModel().insert(ass);
       User awww= UserModel().query(1);
